b37c1c19570a923a56120b78fa6cd6ad784ea416
Jakub Vrana <jvrana@monetplus.cz>
2018-11-22 11:02:05 +0100

    Using Spring to Receive JMS Message

---
+++ src/main/java/guru/springframework/services/jms/TextMessageListener.java
@@ -0,0 +1,19 @@
+package guru.springframework.services.jms;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TextMessageListener {
+
+    @JmsListener(destination = "text.messagequeue")
+    public void onMessage(String msg) {
+        System.out.println("##########################");
+        System.out.println("##########################");
+        System.out.println("I GOT A MESSAGE");
+        System.out.println(msg);
+        System.out.println("##########################");
+        System.out.println("##########################");
+    }
+
+}
